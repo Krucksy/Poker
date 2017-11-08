09cebded0d8b6936892a3353490d04241113348e
Krucksy <krucksy@gmail.com>
2017-11-08 20:09:49 +0100

    Création jeu de carte

---
+++ script.js
@@ -0,0 +1,92 @@
+let COULEUR = {
+    COEUR,
+    PIQUE,
+    CARREAU,
+    TREFLE    
+}
+
+let TYPE = {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    J,
+    Q,
+    K,
+    AS
+}
+
+class carte {
+    constructor(couleur, type) {
+        this.couleur = couleur
+        this.type = type
+    }
+}
+
+
+var Co1 = new carte(COULEUR.COEUR,TYPE.ONE)
+var Co2 = new carte(COULEUR.COEUR,TYPE.TWO)
+var Co3 = new carte(COULEUR.COEUR,TYPE.THREE)
+var Co4 = new carte(COULEUR.COEUR,TYPE.FOUR)
+var Co5 = new carte(COULEUR.COEUR,TYPE.FIVE)
+var Co6 = new carte(COULEUR.COEUR,TYPE.SIX)
+var Co7 = new carte(COULEUR.COEUR,TYPE.SEVEN)
+var Co8 = new carte(COULEUR.COEUR,TYPE.EIGHT)
+var Co9 = new carte(COULEUR.COEUR,TYPE.NINE)
+var Co10 = new carte(COULEUR.COEUR,TYPE.TEN)
+var CoJ = new carte(COULEUR.COEUR,TYPE.J)
+var CoQ = new carte(COULEUR.COEUR,TYPE.Q)
+var CoK = new carte(COULEUR.COEUR,TYPE.K)
+var CoAS = new carte(COULEUR.COEUR,TYPE.AS)
+
+var P1 = new carte(COULEUR.PIQUE,TYPE.ONE)
+var P2 = new carte(COULEUR.PIQUE,TYPE.TWO)
+var P3 = new carte(COULEUR.PIQUE,TYPE.THREE)
+var P4 = new carte(COULEUR.PIQUE,TYPE.FOUR)
+var P5 = new carte(COULEUR.PIQUE,TYPE.FIVE)
+var P6 = new carte(COULEUR.PIQUE,TYPE.SIX)
+var P7 = new carte(COULEUR.PIQUE,TYPE.SEVEN)
+var P8 = new carte(COULEUR.PIQUE,TYPE.EIGHT)
+var P9 = new carte(COULEUR.PIQUE,TYPE.NINE)
+var P10 = new carte(COULEUR.PIQUE,TYPE.TEN)
+var PJ = new carte(COULEUR.PIQUE,TYPE.J)
+var PQ = new carte(COULEUR.PIQUE,TYPE.Q)
+var PK = new carte(COULEUR.PIQUE,TYPE.K)
+var PAS = new carte(COULEUR.PIQUE,TYPE.AS)
+
+var Ca1 = new carte(COULEUR.CARREAU,TYPE.ONE)
+var Ca2 = new carte(COULEUR.CARREAU,TYPE.TWO)
+var Ca3 = new carte(COULEUR.CARREAU,TYPE.THREE)
+var Ca4 = new carte(COULEUR.CARREAU,TYPE.FOUR)
+var Ca5 = new carte(COULEUR.CARREAU,TYPE.FIVE)
+var Ca6 = new carte(COULEUR.CARREAU,TYPE.SIX)
+var Ca7 = new carte(COULEUR.CARREAU,TYPE.SEVEN)
+var Ca8 = new carte(COULEUR.CARREAU,TYPE.EIGHT)
+var Ca9 = new carte(COULEUR.CARREAU,TYPE.NINE)
+var Ca10 = new carte(COULEUR.CARREAU,TYPE.TEN)
+var CaJ = new carte(COULEUR.CARREAU,TYPE.J)
+var CaQ = new carte(COULEUR.CARREAU,TYPE.Q)
+var CaK = new carte(COULEUR.CARREAU,TYPE.K)
+var CaAS = new carte(COULEUR.CARREAU,TYPE.AS)
+
+var T1 = new carte(COULEUR.TREFLE,TYPE.ONE)
+var T2 = new carte(COULEUR.TREFLE,TYPE.TWO)
+var T3 = new carte(COULEUR.TREFLE,TYPE.THREE)
+var T4 = new carte(COULEUR.TREFLE,TYPE.FOUR)
+var T5 = new carte(COULEUR.TREFLE,TYPE.FIVE)
+var T6 = new carte(COULEUR.TREFLE,TYPE.SIX)
+var T7 = new carte(COULEUR.TREFLE,TYPE.SEVEN)
+var T8 = new carte(COULEUR.TREFLE,TYPE.EIGHT)
+var T9 = new carte(COULEUR.TREFLE,TYPE.NINE)
+var T10 = new carte(COULEUR.TREFLE,TYPE.TEN)
+var TJ = new carte(COULEUR.TREFLE,TYPE.J)
+var TQ = new carte(COULEUR.TREFLE,TYPE.Q)
+var TK = new carte(COULEUR.TREFLE,TYPE.K)
+var TAS = new carte(COULEUR.TREFLE,TYPE.AS)
+
