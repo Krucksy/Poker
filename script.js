221b7d0c48a0a683187e12d995521d0b1db42cb9
Plumsim <Simon.Plumey@divtec.ch>
2017-11-10 09:20:10 +0100

    Bug fix

--- script.js
+++ script.js
@@ -1,92 +1,97 @@
-let COULEUR = {
-    COEUR,
-    PIQUE,
-    CARREAU,
-    TREFLE    
+const COULEUR = {
+    "COEUR": 0,
+    "PIQUE": 1,
+    "CARREAU": 2,
+    "TREFLE": 3    
 }
 
-let TYPE = {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    J,
-    Q,
-    K,
-    AS
+const TYPE = {
+    "ONE": 0,
+    "TWO": 1,
+    "THREE": 2,
+    "FOUR": 3,
+    "FIVE": 4,
+    "SIX": 5,
+    "SEVEN": 6,
+    "EIGHT": 7,
+    "NINE": 8,
+    "TEN": 9,
+    "J": 10,
+    "Q": 11,
+    "K": 12,
+    "AS": 13
 }
 
-class carte {
+class Carte {
+    /**
+     * 
+     * @param {COULEUR} couleur 
+     * @param {TYPE} type 
+     */
     constructor(couleur, type) {
+        this.uhsuz = 0
         this.couleur = couleur
         this.type = type
     }
 }
 
 
-var Co1 = new carte(COULEUR.COEUR,TYPE.ONE)
-var Co2 = new carte(COULEUR.COEUR,TYPE.TWO)
-var Co3 = new carte(COULEUR.COEUR,TYPE.THREE)
-var Co4 = new carte(COULEUR.COEUR,TYPE.FOUR)
-var Co5 = new carte(COULEUR.COEUR,TYPE.FIVE)
-var Co6 = new carte(COULEUR.COEUR,TYPE.SIX)
-var Co7 = new carte(COULEUR.COEUR,TYPE.SEVEN)
-var Co8 = new carte(COULEUR.COEUR,TYPE.EIGHT)
-var Co9 = new carte(COULEUR.COEUR,TYPE.NINE)
-var Co10 = new carte(COULEUR.COEUR,TYPE.TEN)
-var CoJ = new carte(COULEUR.COEUR,TYPE.J)
-var CoQ = new carte(COULEUR.COEUR,TYPE.Q)
-var CoK = new carte(COULEUR.COEUR,TYPE.K)
-var CoAS = new carte(COULEUR.COEUR,TYPE.AS)
+const Co1 = new Carte(COULEUR.COEUR,TYPE.ONE)
+const Co2 = new Carte(COULEUR.COEUR,TYPE.TWO)
+const Co3 = new Carte(COULEUR.COEUR,TYPE.THREE)
+const Co4 = new Carte(COULEUR.COEUR,TYPE.FOUR)
+const Co5 = new Carte(COULEUR.COEUR,TYPE.FIVE)
+const Co6 = new Carte(COULEUR.COEUR,TYPE.SIX)
+const Co7 = new Carte(COULEUR.COEUR,TYPE.SEVEN)
+const Co8 = new Carte(COULEUR.COEUR,TYPE.EIGHT)
+const Co9 = new Carte(COULEUR.COEUR,TYPE.NINE)
+const Co10 = new Carte(COULEUR.COEUR,TYPE.TEN)
+const CoJ = new Carte(COULEUR.COEUR,TYPE.J)
+const CoQ = new Carte(COULEUR.COEUR,TYPE.Q)
+const CoK = new Carte(COULEUR.COEUR,TYPE.K)
+const CoAS = new Carte(COULEUR.COEUR,TYPE.AS)
 
-var P1 = new carte(COULEUR.PIQUE,TYPE.ONE)
-var P2 = new carte(COULEUR.PIQUE,TYPE.TWO)
-var P3 = new carte(COULEUR.PIQUE,TYPE.THREE)
-var P4 = new carte(COULEUR.PIQUE,TYPE.FOUR)
-var P5 = new carte(COULEUR.PIQUE,TYPE.FIVE)
-var P6 = new carte(COULEUR.PIQUE,TYPE.SIX)
-var P7 = new carte(COULEUR.PIQUE,TYPE.SEVEN)
-var P8 = new carte(COULEUR.PIQUE,TYPE.EIGHT)
-var P9 = new carte(COULEUR.PIQUE,TYPE.NINE)
-var P10 = new carte(COULEUR.PIQUE,TYPE.TEN)
-var PJ = new carte(COULEUR.PIQUE,TYPE.J)
-var PQ = new carte(COULEUR.PIQUE,TYPE.Q)
-var PK = new carte(COULEUR.PIQUE,TYPE.K)
-var PAS = new carte(COULEUR.PIQUE,TYPE.AS)
+const P1 = new Carte(COULEUR.PIQUE,TYPE.ONE)
+const P2 = new Carte(COULEUR.PIQUE,TYPE.TWO)
+const P3 = new Carte(COULEUR.PIQUE,TYPE.THREE)
+const P4 = new Carte(COULEUR.PIQUE,TYPE.FOUR)
+const P5 = new Carte(COULEUR.PIQUE,TYPE.FIVE)
+const P6 = new Carte(COULEUR.PIQUE,TYPE.SIX)
+const P7 = new Carte(COULEUR.PIQUE,TYPE.SEVEN)
+const P8 = new Carte(COULEUR.PIQUE,TYPE.EIGHT)
+const P9 = new Carte(COULEUR.PIQUE,TYPE.NINE)
+const P10 = new Carte(COULEUR.PIQUE,TYPE.TEN)
+const PJ = new Carte(COULEUR.PIQUE,TYPE.J)
+const PQ = new Carte(COULEUR.PIQUE,TYPE.Q)
+const PK = new Carte(COULEUR.PIQUE,TYPE.K)
+const PAS = new Carte(COULEUR.PIQUE,TYPE.AS)
 
-var Ca1 = new carte(COULEUR.CARREAU,TYPE.ONE)
-var Ca2 = new carte(COULEUR.CARREAU,TYPE.TWO)
-var Ca3 = new carte(COULEUR.CARREAU,TYPE.THREE)
-var Ca4 = new carte(COULEUR.CARREAU,TYPE.FOUR)
-var Ca5 = new carte(COULEUR.CARREAU,TYPE.FIVE)
-var Ca6 = new carte(COULEUR.CARREAU,TYPE.SIX)
-var Ca7 = new carte(COULEUR.CARREAU,TYPE.SEVEN)
-var Ca8 = new carte(COULEUR.CARREAU,TYPE.EIGHT)
-var Ca9 = new carte(COULEUR.CARREAU,TYPE.NINE)
-var Ca10 = new carte(COULEUR.CARREAU,TYPE.TEN)
-var CaJ = new carte(COULEUR.CARREAU,TYPE.J)
-var CaQ = new carte(COULEUR.CARREAU,TYPE.Q)
-var CaK = new carte(COULEUR.CARREAU,TYPE.K)
-var CaAS = new carte(COULEUR.CARREAU,TYPE.AS)
+const Ca1 = new Carte(COULEUR.CARREAU,TYPE.ONE)
+const Ca2 = new Carte(COULEUR.CARREAU,TYPE.TWO)
+const Ca3 = new Carte(COULEUR.CARREAU,TYPE.THREE)
+const Ca4 = new Carte(COULEUR.CARREAU,TYPE.FOUR)
+const Ca5 = new Carte(COULEUR.CARREAU,TYPE.FIVE)
+const Ca6 = new Carte(COULEUR.CARREAU,TYPE.SIX)
+const Ca7 = new Carte(COULEUR.CARREAU,TYPE.SEVEN)
+const Ca8 = new Carte(COULEUR.CARREAU,TYPE.EIGHT)
+const Ca9 = new Carte(COULEUR.CARREAU,TYPE.NINE)
+const Ca10 = new Carte(COULEUR.CARREAU,TYPE.TEN)
+const CaJ = new Carte(COULEUR.CARREAU,TYPE.J)
+const CaQ = new Carte(COULEUR.CARREAU,TYPE.Q)
+const CaK = new Carte(COULEUR.CARREAU,TYPE.K)
+const CaAS = new Carte(COULEUR.CARREAU,TYPE.AS)
 
-var T1 = new carte(COULEUR.TREFLE,TYPE.ONE)
-var T2 = new carte(COULEUR.TREFLE,TYPE.TWO)
-var T3 = new carte(COULEUR.TREFLE,TYPE.THREE)
-var T4 = new carte(COULEUR.TREFLE,TYPE.FOUR)
-var T5 = new carte(COULEUR.TREFLE,TYPE.FIVE)
-var T6 = new carte(COULEUR.TREFLE,TYPE.SIX)
-var T7 = new carte(COULEUR.TREFLE,TYPE.SEVEN)
-var T8 = new carte(COULEUR.TREFLE,TYPE.EIGHT)
-var T9 = new carte(COULEUR.TREFLE,TYPE.NINE)
-var T10 = new carte(COULEUR.TREFLE,TYPE.TEN)
-var TJ = new carte(COULEUR.TREFLE,TYPE.J)
-var TQ = new carte(COULEUR.TREFLE,TYPE.Q)
-var TK = new carte(COULEUR.TREFLE,TYPE.K)
-var TAS = new carte(COULEUR.TREFLE,TYPE.AS)
-
+const T1 = new Carte(COULEUR.TREFLE,TYPE.ONE)
+const T2 = new Carte(COULEUR.TREFLE,TYPE.TWO)
+const T3 = new Carte(COULEUR.TREFLE,TYPE.THREE)
+const T4 = new Carte(COULEUR.TREFLE,TYPE.FOUR)
+const T5 = new Carte(COULEUR.TREFLE,TYPE.FIVE)
+const T6 = new Carte(COULEUR.TREFLE,TYPE.SIX)
+const T7 = new Carte(COULEUR.TREFLE,TYPE.SEVEN)
+const T8 = new Carte(COULEUR.TREFLE,TYPE.EIGHT)
+const T9 = new Carte(COULEUR.TREFLE,TYPE.NINE)
+const T10 = new Carte(COULEUR.TREFLE,TYPE.TEN)
+const TJ = new Carte(COULEUR.TREFLE,TYPE.J)
+const TQ = new Carte(COULEUR.TREFLE,TYPE.Q)
+const TK = new Carte(COULEUR.TREFLE,TYPE.K)
+const TAS = new Carte(COULEUR.TREFLE,TYPE.AS)
